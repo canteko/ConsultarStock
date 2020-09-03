e8f97d8ea57dc9f4f0d64a267534e7b43a5ec9e5
Canteko <canteko@gmail.com>
2020-09-04 00:39:41 +0200

    first commit

---
+++ listener.py
@@ -0,0 +1,23 @@
+import telebot
+from pyquery import PyQuery as pq
+import time
+users={}
+bot=telebot.TeleBot("1310363512:AAEvI11_M_yuAEY3_Vg9VbpwCW0_5xi3UmU")
+
+while(1):
+    plates = pq(url='https://www.decathlon.es/es/p/disco-de-fundicion-28-mm-musculacion-0-5-kg-a-20-kg-domyos-cross-fitness/_/R-p-7278?mc=1042303&c=NEGRO/')
+
+    stock10kg = plates('.sizes__info:contains("10 KG")').parent().find('.sizes__stock').find('.sizes__stock__info').html()
+    stock5kg = plates('.sizes__info:contains("5 KG")').parent().find('.sizes__stock').find('.sizes__stock__info').html()
+
+    if(stock10kg != "QUEDAN 0"):
+        string_stock = "Discos de 10Kg disponibles, mensaje de stock: '" + stock10kg + "'"  + " " +  "https://www.decathlon.es/es/p/disco-de-fundicion-28-mm-musculacion-0-5-kg-a-20-kg-domyos-cross-fitness/_/R-p-7278?mc=1042303&c=NEGRO/"
+        bot.send_message(77771278, string_stock)
+        bot.send_message(324294249, string_stock)
+        bot.send_message(8268229, string_stock)
+
+    if(stock5kg != "QUEDAN 0"):
+        string_stock = "Discos de 5Kg disponibles, mensaje de stock: '" + stock5kg + "'" + " " +  "https://www.decathlon.es/es/p/disco-de-fundicion-28-mm-musculacion-0-5-kg-a-20-kg-domyos-cross-fitness/_/R-p-7278?mc=1042303&c=NEGRO/"
+        bot.send_message(8268229, string_stock)
+
+    time.sleep(10)
---
+++ main.py
@@ -0,0 +1,16 @@
+import telebot
+from pyquery import PyQuery as pq
+users={}
+bot=telebot.TeleBot("1310363512:AAEvI11_M_yuAEY3_Vg9VbpwCW0_5xi3UmU")
+
+def listener(messages):
+    for m in messages:
+        plid=m.chat.id
+        print(plid)
+        if m.content_type=="text":
+            if not m.text[0]=='/' and str(plid) not in users:
+            		bot.send_message(plid, "To begin, type '/start'") 
+
+bot.set_update_listener(listener)
+
+bot.polling()           
