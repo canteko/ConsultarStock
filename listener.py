c0199c5c0eee57c788f9846dbc8bd8ea64e68bb8
Canteko <canteko@gmail.com>
2020-09-04 00:53:00 +0200

    cambio

--- listener.py
+++ listener.py
@@ -4,6 +4,8 @@ import time
 users={}
 bot=telebot.TeleBot("1310363512:AAEvI11_M_yuAEY3_Vg9VbpwCW0_5xi3UmU")
 
+bot.send_message(77771278, "Correcto")
+
 while(1):
     plates = pq(url='https://www.decathlon.es/es/p/disco-de-fundicion-28-mm-musculacion-0-5-kg-a-20-kg-domyos-cross-fitness/_/R-p-7278?mc=1042303&c=NEGRO/')
 
@@ -19,7 +21,5 @@ while(1):
     if(stock5kg != "QUEDAN 0"):
         string_stock = "Discos de 5Kg disponibles, mensaje de stock: '" + stock5kg + "'" + " " +  "https://www.decathlon.es/es/p/disco-de-fundicion-28-mm-musculacion-0-5-kg-a-20-kg-domyos-cross-fitness/_/R-p-7278?mc=1042303&c=NEGRO/"
         bot.send_message(8268229, string_stock)
-
-    bot.send_message(77771278, "Correcto")
 
     time.sleep(10)
