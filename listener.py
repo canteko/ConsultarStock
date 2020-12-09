22c5adafd3e7c8e385f497313e2e60723cbf8ea2
Francisco Sáenz <canteko@gmail.com>
2020-12-09 20:09:22 +0100

    cambios

--- listener.py
+++ listener.py
@@ -3,6 +3,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
+from pprint import pprint
 
 #Importamos la key del bot
 bot=telebot.TeleBot("1310363512:AAEvI11_M_yuAEY3_Vg9VbpwCW0_5xi3UmU")
@@ -19,15 +20,19 @@ while(keepTrying):
     driver.find_element_by_xpath('//settings-ui').send_keys(Keys.ENTER)
 
     #Entramos en la web y obtenemos el body
-    driver.get('https://www.decathlon.es/es/p/kettlebell-pesa-rusa-8kg-domyos-cross-fitness-musculacion/_/R-p-152873')
-    # driver.get('https://www.decathlon.es/es/p/rack-cross-training-musculacion-domyos-500-squat-traction/_/R-p-158534?mc=8380452')
-    cookieElement = driver.find_element_by_id("didomi-notice-agree-button")
-    cookieElement.click()
-    element = driver.find_element_by_id("ctaButton")
-    if(element.is_displayed() == True):
-        bot.send_message(77771278, "Hay")
-        # element.click()
-        driver.execute_script("arguments[0].click();", element) 
-        keepTrying = False
+    #driver.get('https://www.decathlon.es/es/p/kettlebell-pesa-rusa-8kg-domyos-cross-fitness-musculacion/_/R-p-152873')
+    driver.get('https://www.decathlon.es/es/p/rack-cross-training-musculacion-domyos-500-squat-traction/_/R-p-158534?mc=8380452')
+    try:
+        cookieElement = driver.find_element_by_id("didomi-notice-agree-button")
+    finally:
+        if(cookieElement.is_displayed() == True):
+            driver.execute_script("arguments[0].click();", cookieElement)
+    try:
+        element = driver.find_element_by_id("ctaButton")
+    finally:
+        if(element.is_displayed() == True):
+            bot.send_message(77771278, "https://www.decathlon.es/es/p/rack-cross-training-musculacion-domyos-500-squat-traction/_/R-p-158534?mc=8380452")
+            driver.execute_script("arguments[0].click();", element) 
+            keepTrying = False
 
-    time.sleep(10)
+    time.sleep(5)
