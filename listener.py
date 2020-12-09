3416ee21ca521c4c725de41e77cb8c0352394c70
Francisco <francisco.saenz@kosaas.com>
2020-12-09 20:49:38 +0100

    Comentarios

--- listener.py
+++ listener.py
@@ -13,6 +13,8 @@ bot=telebot.TeleBot("1310363512:AAEvI11_M_yuAEY3_Vg9VbpwCW0_5xi3UmU")
 #Abrimos el navegador
 driver = webdriver.Chrome(ChromeDriverManager().install())
 keepTrying = True
+clickCookie = True
+clickCart = True
 
 #Bucle infinito
 while(keepTrying):
@@ -20,22 +22,29 @@ while(keepTrying):
     driver.get('chrome://settings/clearBrowserData')
     driver.find_element_by_xpath('//settings-ui').send_keys(Keys.ENTER)
 
+    #Intentamos clicar de nuevo
+    clickCookie = True
+    clickCart = True
+
     #Entramos en la web y obtenemos el body
-    #driver.get('https://www.decathlon.es/es/p/kettlebell-pesa-rusa-8kg-domyos-cross-fitness-musculacion/_/R-p-152873')
     driver.get('https://www.decathlon.es/es/p/rack-cross-training-musculacion-domyos-500-squat-traction/_/R-p-158534?mc=8380452')
+
+    #Intentamos pulsar el botón para aceptar las cookies
     try:
         cookieElement = driver.find_element_by_id("didomi-notice-agree-button")
     except:
-        print("NO")
+        clickCookie = False
     finally:
-        if(cookieElement.is_displayed() == True):
+        if(clickCookie == True and cookieElement.is_displayed() == True):
             driver.execute_script("arguments[0].click();", cookieElement)
+
+    #Intentamos pulsar el botón de añadir el carrito, si lo puslamos, paramos los intentos y mandamos mensaje al telegram
     try:
         element = driver.find_element_by_id("ctaButton")
     except:
-        print("NO")
+        clickCart = False
     finally:
-        if(element.is_displayed() == True):
+        if(clickCart == True and element.is_displayed() == True):
             bot.send_message(77771278, "https://www.decathlon.es/es/p/rack-cross-training-musculacion-domyos-500-squat-traction/_/R-p-158534?mc=8380452")
             driver.execute_script("arguments[0].click();", element) 
             keepTrying = False
