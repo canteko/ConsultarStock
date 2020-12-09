daf4afd6ff72f64bea9ef95c8af3260392071cc2
Canteko <canteko@gmail.com>
2020-12-09 20:38:45 +0100

    driver

--- listener.py
+++ listener.py
@@ -3,14 +3,15 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from pprint import pprint
 
 #Importamos la key del bot
 bot=telebot.TeleBot("1310363512:AAEvI11_M_yuAEY3_Vg9VbpwCW0_5xi3UmU")
 
 #Abrimos el navegador
-#driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
-driver = webdriver.Chrome()  
+driver = webdriver.Chrome(ChromeDriverManager().install())
 keepTrying = True
 
 #Bucle infinito
@@ -24,11 +25,15 @@ while(keepTrying):
     driver.get('https://www.decathlon.es/es/p/rack-cross-training-musculacion-domyos-500-squat-traction/_/R-p-158534?mc=8380452')
     try:
         cookieElement = driver.find_element_by_id("didomi-notice-agree-button")
+    except:
+        print("NO")
     finally:
         if(cookieElement.is_displayed() == True):
             driver.execute_script("arguments[0].click();", cookieElement)
     try:
         element = driver.find_element_by_id("ctaButton")
+    except:
+        print("NO")
     finally:
         if(element.is_displayed() == True):
             bot.send_message(77771278, "https://www.decathlon.es/es/p/rack-cross-training-musculacion-domyos-500-squat-traction/_/R-p-158534?mc=8380452")
