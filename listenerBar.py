999cd3f340d6595b65a7f29253b6e4dfc2dbe72e
Francisco Sáenz <canteko@gmail.com>
2020-12-09 22:54:26 +0100

    listener bar

---
+++ listenerBar.py
@@ -0,0 +1,52 @@
+import telebot
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from pprint import pprint
+
+#Importamos la key del bot
+bot=telebot.TeleBot("1310363512:AAEvI11_M_yuAEY3_Vg9VbpwCW0_5xi3UmU")
+
+#Abrimos el navegador
+driver = webdriver.Chrome(ChromeDriverManager().install())
+keepTrying = True
+clickCookie = True
+clickCart = True
+
+#Bucle infinito
+while(keepTrying):
+    #Limpiamos cache y cookies, por si acaso estamos consultando todo el rato una página cacheada
+    driver.get('chrome://settings/clearBrowserData')
+    driver.find_element_by_xpath('//settings-ui').send_keys(Keys.ENTER)
+
+    #Intentamos clicar de nuevo
+    clickCookie = True
+    clickCart = True
+
+    #Entramos en la web y obtenemos el body
+    driver.get('https://www.decathlon.es/es/p/barra-de-musculacion-domyos-de-2-m-y-28-mm/_/R-p-9291?mc=8289900')
+
+    #Intentamos pulsar el botón para aceptar las cookies
+    try:
+        cookieElement = driver.find_element_by_id("didomi-notice-agree-button")
+    except:
+        clickCookie = False
+    finally:
+        if(clickCookie == True and cookieElement.is_displayed() == True):
+            driver.execute_script("arguments[0].click();", cookieElement)
+
+    #Intentamos pulsar el botón de añadir el carrito, si lo puslamos, paramos los intentos y mandamos mensaje al telegram
+    try:
+        element = driver.find_element_by_id("ctaButton")
+    except:
+        clickCart = False
+    finally:
+        if(clickCart == True and element.is_displayed() == True):
+            bot.send_message(77771278, "https://www.decathlon.es/es/p/barra-de-musculacion-domyos-de-2-m-y-28-mm/_/R-p-9291?mc=8289900")
+            driver.execute_script("arguments[0].click();", element) 
+            keepTrying = False
+
+    time.sleep(5)
